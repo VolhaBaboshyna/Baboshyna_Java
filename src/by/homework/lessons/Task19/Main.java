3b19728c04766da48771f343abe7f0ee65134f96
VolhaBaboshyna <volha.baboshyna@gmail.com>
2026-06-30 21:59:18 +0300

    домашка19

---
+++ src/by/homework/lessons/Task19/Main.java
@@ -0,0 +1,13 @@
+package by.homework.lessons.Task19;
+
+public class Main {
+    public static void main(String[] args) {
+        String resultToUpper = modify(str -> str.toUpperCase(), "hello");
+        System.out.println(resultToUpper);
+    };
+}
+
+private static String modify(ToUpper toUpper, String value) {
+    return toUpper.modify(value);
+
+}
