5d931ccd3bd66ce982aaedc1a6766e9b5a296a4b
VolhaBaboshyna <volha.baboshyna@gmail.com>
2026-07-01 16:38:06 +0300

    домашка19

--- src/by/homework/lessons/Task19/Main.java
+++ src/by/homework/lessons/Task19/Main.java
@@ -1,6 +1,8 @@
 package by.homework.lessons.Task19;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +10,9 @@ public class Main {
         String resultToUpper = modify(str -> str.toUpperCase(), srtToModify);
         System.out.println(resultToUpper);
 
-        System.out.print("Введите континент: ");
-        String continent = scanner.nextLine();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите континент");
+//        String continent = sc.nextLine();
 
         City minsk = new City(1, "Minsk", 20546, "Europe");
         City gomel = new City(2, "Gomel", 7896, "Europe");
@@ -19,21 +22,59 @@ public class Main {
 
         List<City> cityList = List.of(minsk, gomel, moscow, pekin, deli);
 
-        List<City> cityListEurope = cityList.stream().filter(city -> {
-            if (city.getContinent().equals("Europe")) {
-                return true;
-            }
-            return false;
+//        List<City> cityListContinent = cityList.stream().filter(city -> {
+//            if (city.getContinent().equals(continent)) {
+//                return true;
+//            }
+//            return false;
+//        }).toList();
+//        System.out.println("Города выбранного континента " + continent);
+//        System.out.println(cityListContinent);
+
+//        Optional<City> cityListContMaxPop = cityListContinent.stream().max((city1, city2) ->
+//                Integer.compare(city1.getPopulation(), city2.getPopulation()));
+//
+//        Optional<City> cityListContMinPop = cityListContinent.stream().min((city1, city2) ->
+//                Integer.compare(city1.getPopulation(), city2.getPopulation()));
+
+
+        Optional<City> cityListMaxPop = cityList.stream().max((city1, city2) ->
+                Integer.compare(city1.getPopulation(), city2.getPopulation()));
+
+        Optional<City> cityListMinPop = cityList.stream().min((city1, city2) ->
+                Integer.compare(city1.getPopulation(), city2.getPopulation()));
+
+//        System.out.println("Город с максимальным населением на континенте " + continent);
+//        System.out.println(cityListContMaxPop);
+//
+//        System.out.println("Город с минимальным населением на континенте " + continent);
+//        System.out.println(cityListContMinPop);
+//
+//        System.out.println("Город с максимальным населением");
+//        System.out.println(cityListMaxPop);
+//
+//        System.out.println("Город с минимальным населением");
+//        System.out.println(cityListMinPop);
+
+
+        System.out.println("Первый город с населением больше миллиона");
+        Optional<City> cityFirstMoreMillion = cityList.stream().filter(city -> city.getPopulation() > 1000000).findFirst();
+        System.out.println(cityFirstMoreMillion);
+
+        System.out.println("Города с населением больше миллиона");
+        List<City> cityMoreMillion = cityList.stream()
+                .filter(city -> city.getPopulation() >= 1000000)
+                .toList();
+        System.out.print(cityMoreMillion);
+
+        Collection<String> cityNames = cityList.stream().map(city -> {
+            String cityName = city.getName();
+            return cityName;
         }).toList();
 
-        List<City> cityListMaxPop = cityListEurope.stream().max((city1, city2) -> {
-
-        }
-
-
+        System.out.println("Названия всех городов");
+        System.out.println(cityNames);
     }
-
-    ;
 
     private static String modify(ToUpper toUpper, String value) {
         return toUpper.modify(value);
